665e515ad10058a2740837958a35056eff1cf6dc
Rainsong-fire <83505227+Rainsong-fire@users.noreply.github.com>
2022-06-30 10:30:02 +0800

    new

---
+++ src/main/java/com/hjy/hjy/HjyApplication.java
@@ -0,0 +1,13 @@
+package com.hjy.hjy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HjyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HjyApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/hjy/hjy/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.hjy.hjy;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(HjyApplication.class);
+    }
+
+}
